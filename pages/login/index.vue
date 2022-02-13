0afc2f468b35a8cf5155fa2277d92acdb3f862b7
herbert <1577759006@qq.com>
2022-02-13 17:01:16 +0800

    登录注册-封装请求方法

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -6,20 +6,20 @@
           <h1 class="text-xs-center">{{ isLogin ? 'Sign in' : 'Sign up' }}</h1>
           <p class="text-xs-center">
             <nuxt-link v-if="isLogin" to="/register">Need an account?</nuxt-link>
-              <nuxt-link v-else to="/login">Have an account?</nuxt-link>
+            <nuxt-link v-else to="/login">Have an account?</nuxt-link>
           </p>
           <ul class="error-messages">
             <li>That email is invalid.</li>
           </ul>
-          <form>
+          <form @submit.prevent="onSubmit">
             <fieldset v-if="!isLogin" class="form-group">
               <input class="form-control form-control-lg" type="text" placeholder="Your Name">
             </fieldset>
             <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="email" placeholder="Email">
+              <input class="form-control form-control-lg" type="email" placeholder="Email" v-model="user.email">
             </fieldset>
             <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="password" placeholder="Password">
+              <input class="form-control form-control-lg" type="password" placeholder="Password" v-model="user.password">
             </fieldset>
             <button class="btn btn-lg btn-primary pull-xs-right">
               {{ isLogin ? 'Sign in' : 'Sign up' }}
@@ -32,19 +32,34 @@
 </template>
 
 <script>
+import { login } from '@/api/user'
 export default {
   props: {},
   data() {
-    return {}
+    return {
+      user: {
+        email: '',
+        password: '',
+      },
+    }
   },
   computed: {
     isLogin() {
       return this.$route.name === 'login'
-    }
+    },
   },
   created() {},
   mounted() {},
-  methods: {},
+  methods: {
+    async onSubmit() {
+      // 提交表单
+      const { data } = await login({
+          user: this.user
+      })
+      // 登录成功跳转到首页
+      this.$router.push('/')
+    },
+  },
 }
 </script>
 
--- utils/request.js
+++ utils/request.js
@@ -7,3 +7,5 @@ import axios from 'axios'
 const request = axios.create({
   baseURL: 'https://conduit.productionready.io'
 })
+
+export default request
