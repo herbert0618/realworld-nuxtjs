da86c347d54dd4fdf127bcaedf5e3af42d8fdd3d
herbert <1577759006@qq.com>
2022-02-13 13:45:03 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>首页</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
